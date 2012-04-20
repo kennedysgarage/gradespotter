f1631ea6cfa5ce7a8927745b3c4927fd7e9558f0
Christopher Kennedy <kennedysgarage@gmail.com>
2012-04-20 10:01:59 -0400

    updating dates

--- blog/blog.php
+++ blog/blog.php
@@ -17,14 +17,14 @@
 			<h2>Blog</h2>
 			<hr/>
 			<ul id="posts">
-				<li>
+				<!--<li>
 					<h4><a href="/blog/radio-commercial">Radio Commercial</a></h4>
 					<p class="timestamp">April 17th, 2012 by Kennedy</p>
 					<p><iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F42942935&amp;auto_play=false&amp;show_artwork=false&amp;color=54abd0"></iframe></p>
-				</li>
+				</li>-->
 				<li>
 					<h4><a href="/blog/our-journey">Our Journey</a></h4>
-					<p class="timestamp">April 16th, 2012 by Kennedy</p>
+					<p class="timestamp">April 20th, 2012 by Kennedy</p>
 					<p>Grade Spotter is a full-featured restaurant inspection guide for your iPhone. Whether it’s a five-star restaurant or a hole-in-the-wall secret, each New York City restaurant must adhere to a standard of rules and regulations when dealing with food. With over 24,000 restaurants in the city one must question a restaurant’s sanitary principles at one point or another. Separate from taste and appearance, what food actually deserves an A and what food should be thrown in the garbage? Grade Spotter provides the NYC Health Department Restaurant Inspection Grades in a one-stop, user-friendly format and helps you make an informed decision on where to go.</p>
 				</li>
 			</ul>
